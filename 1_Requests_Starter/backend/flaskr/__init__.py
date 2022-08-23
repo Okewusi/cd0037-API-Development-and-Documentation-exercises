c3043928e5a1e4bae4bf9cb4edbc541719c47431
Oluwapelumi Okewusi <pelcool@Oluwapelumis-MacBook-Pro.local>
2022-08-23 10:44:23 -0500

    view all books, update book, delete book and create new book APIs created

--- 1_Requests_Starter/backend/flaskr/__init__.py
+++ 1_Requests_Starter/backend/flaskr/__init__.py
@@ -1,3 +1,4 @@
+from crypt import methods
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy  # , or_
@@ -39,21 +40,114 @@ def create_app(test_config=None):
     #         Response body keys: 'success', 'books' and 'total_books'
     # TEST: When completed, the webpage will display books including title, author, and rating shown as stars
 
+    @app.route('/books')
+    def get_books():
+        books = Book.query.order_by(Book.id).all()
+        formatted_books = [book.format() for book in books]
+        page = request.args.get("page", 1, type=int)
+        start = (page -1)* BOOKS_PER_SHELF
+        end = start + BOOKS_PER_SHELF
+        current_books = formatted_books[start:end]
+
+        if len(current_books) == 0:
+            abort(404)
+
+        return jsonify({
+            "success": True,
+            "books": current_books,
+            "total_books" : len(formatted_books)
+        })
+
     # @TODO: Write a route that will update a single book's rating.
     #         It should only be able to update the rating, not the entire representation
     #         and should follow API design principles regarding method and route.
     #         Response body keys: 'success'
     # TEST: When completed, you will be able to click on stars to update a book's rating and it will persist after refresh
 
+    @app.route('/books/<int:book_id>', methods=['PATCH'])
+    def update_book(book_id):
+    
+        body = request.get_json()
+        try:
+            book = Book.query.filter_by(id=book_id).one_or_none()
+            if book is None:
+                abort(404)
+            if "rating" in body:
+                book.rating = int(body.get('rating'))
+            book.update()
+
+            return jsonify({
+                "success" : True,
+                "id" : book.id
+            })
+        except:
+            abort(400)
+
+
     # @TODO: Write a route that will delete a single book.
     #        Response body keys: 'success', 'deleted'(id of deleted book), 'books' and 'total_books'
     #        Response body keys: 'success', 'books' and 'total_books'
 
+    @app.route('/books/<int:book_id>', methods=['DELETE'])
+    def delete_book(book_id):
+        try:
+            book = Book.query.filter_by(id=book_id).one_or_none()
+            if book is None:
+                abort(404)
+            book.delete()
+            books = Book.query.order_by(Book.id).all()
+            return jsonify({
+                "success": True,
+                "deleted": book.id,
+                "total_books": len(books)
+            })
+        except:
+            abort(400)
+    
+    @app.errorhandler(400)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message" : "unprocessable"
+        }), 422
     # TEST: When completed, you will be able to delete a single book by clicking on the trashcan.
 
     # @TODO: Write a route that create a new book.
     #        Response body keys: 'success', 'created'(id of created book), 'books' and 'total_books'
     # TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books.
     #       Your new book should show up immediately after you submit it at the end of the page.
+    @app.route('/books', methods=['POST'])
+    def create_book():
+        body = request.get_json()
+
+        new_title = body.get("title", None)
+        new_author = body.get('author', None)
+        new_rating = body.get('rating', None)
+
+        try:
+            book = Book(title=new_title, author=new_author, rating=new_rating)
+            book.insert()
+
+            books = Book.query.order_by(Book.id).all()
+            formatted_books = [book.format() for book in books]
+
+            return jsonify({
+                "success": True,
+                "created" : book.id,
+                "books": formatted_books,
+                "total_books" : len(books)
+            })
+
+        except:
+            abort(422)
 
     return app
